a39b97974ca0f4bed07154ac7d3932f60f599f0e
MCA\dip <dip@mca.dev>
2024-02-04 13:21:40 +0100

    added gallery

--- nx-workspace/src/app/app.routes.ts
+++ nx-workspace/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { GalleryComponent } from '../components/gallery/gallery.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AppComponent },
+    { path: 'gallery', component: GalleryComponent },
+];
