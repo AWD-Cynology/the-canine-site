61d74a8938df2be75d5c48122d86727ddd32b9d1
AndrejDev <andrejpanev8@gmail.com>
2024-04-20 01:15:18 +0200

    Connected Login Angular to .Net API

---
+++ cynology-backend/cynology-backend/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using cynology_backend.Models.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace cynology_backend.Data
+{
+    public class DataContext : IdentityDbContext<CynologyUser>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { 
+
+        }
+    }
+}
---
+++ cynology-backend/cynology-backend/Models/Identity/CynologyUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace cynology_backend.Models.Identity
+{
+    public class CynologyUser : IdentityUser
+    {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Address { get; set; }
+
+    }
+}
