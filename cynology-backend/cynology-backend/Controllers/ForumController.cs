c1b068265dc3fb5a7a21290ff7abc55cb0ad8765
Dimi <popovd209@gmail.com>
2024-04-28 13:45:48 +0200

    changed order of methods

--- cynology-backend/cynology-backend/Controllers/ForumController.cs
+++ cynology-backend/cynology-backend/Controllers/ForumController.cs
@@ -35,6 +35,83 @@ public class ForumController : ControllerBase
             .ToList();
     }
 
+    [HttpPost("new-thread")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public async Task<IActionResult> NewThread([FromBody] ThreadDTO threadDTO)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        string? loggedUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (string.IsNullOrEmpty(loggedUserId))
+        {
+            return BadRequest("User ID not found.");
+        }
+
+        Models.Thread thread = new Models.Thread
+        {
+            Id = Guid.NewGuid(),
+            Topic = threadDTO.Topic,
+            CynologyUserId = loggedUserId,
+            Title = threadDTO.Title,
+            Text = threadDTO.Text,
+            DatePosted = DateTime.Now,
+            Replies = null
+        };
+
+        _dataContext.Threads.Add(thread);
+        try
+        {
+            await _dataContext.SaveChangesAsync();
+        }
+        catch (DbUpdateException ex)
+        {
+            return BadRequest($"Error creating new thread: {ex.Message}");
+        }
+
+        return Ok(thread);
+    }
+
+    [HttpPost("reply-to-thread")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public async Task<IActionResult> ReplyToThread(string threadId, [FromBody] ReplyDTO replyDTO)
+    {
+        var thread = _dataContext.Threads
+            .Where(z => z.Id.Equals(threadId))
+            .FirstOrDefaultAsync();
+
+        string? loggedUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (string.IsNullOrEmpty(loggedUserId))
+        {
+            return BadRequest("User ID not found.");
+        }
+
+        Reply reply = new Reply
+        {
+            Id = Guid.NewGuid(),
+            ThreadId = Guid.Parse(threadId),
+            UserId = Guid.Parse(loggedUserId),
+            Text = replyDTO.Text,
+            DatePosted = replyDTO.DatePosted,
+            CommentToReply = null
+        };
+
+        _dataContext.Replies.Add(reply);
+        try
+        {
+            await _dataContext.SaveChangesAsync();
+        }
+        catch (DbUpdateException ex)
+        {
+            return BadRequest($"Error creating adding new Reply: {ex.Message}");
+        }
+
+        return Ok(reply);
+    }
+
     [HttpPost("comment-to-reply")]
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public async Task<IActionResult> CommentToReply(string commentId, [FromBody] ReplyDTO replyDTO)
@@ -76,82 +153,5 @@ public class ForumController : ControllerBase
         }
 
         return Ok(reply);
-    }
-
-    [HttpPost("reply-to-thread")]
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-    public async Task<IActionResult> ReplyToThread(string threadId, [FromBody] ReplyDTO replyDTO)
-    {
-        var thread = _dataContext.Threads
-            .Where(z => z.Id.Equals(threadId))
-            .FirstOrDefaultAsync();
-
-        string? loggedUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-        if (string.IsNullOrEmpty(loggedUserId))
-        {
-            return BadRequest("User ID not found.");
-        }
-
-        Reply reply = new Reply
-        {
-            Id = Guid.NewGuid(),
-            ThreadId = Guid.Parse(threadId),
-            UserId = Guid.Parse(loggedUserId),
-            Text = replyDTO.Text,
-            DatePosted = replyDTO.DatePosted,
-            CommentToReply = null
-        };
-
-        _dataContext.Replies.Add(reply);
-        try
-        {
-            await _dataContext.SaveChangesAsync();
-        }
-        catch (DbUpdateException ex)
-        {
-            return BadRequest($"Error creating adding new Reply: {ex.Message}");
-        }
-
-        return Ok(reply);
-    }
-
-    [HttpPost("new-thread")]
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-    public async Task<IActionResult> NewThread([FromBody] ThreadDTO threadDTO)
-    {
-        if (!ModelState.IsValid)
-        {
-            return BadRequest(ModelState);
-        }
-
-        string? loggedUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-        if (string.IsNullOrEmpty(loggedUserId))
-        {
-            return BadRequest("User ID not found.");
-        }
-
-        Models.Thread thread = new Models.Thread
-        {
-            Id = Guid.NewGuid(),
-            Topic = threadDTO.Topic,
-            CynologyUserId = loggedUserId,
-            Title = threadDTO.Title,
-            Text = threadDTO.Text,
-            DatePosted = DateTime.Now,
-            Replies = null
-        };
-
-        _dataContext.Threads.Add(thread);
-        try
-        {
-            await _dataContext.SaveChangesAsync();
-        }
-        catch (DbUpdateException ex)
-        {
-            return BadRequest($"Error creating new thread: {ex.Message}");
-        }
-
-        return Ok(thread);
     }
 }
